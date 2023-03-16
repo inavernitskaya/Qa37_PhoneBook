d4b968d9b9186ecde688d4fac0e14f0e5bbe17ac
inavernitskaya <inavernitskaya@gmail.com>
2023-03-16 21:09:13 +0200

    login success

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,44 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        //if Sign Out present----> LogOut
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().Logout();
+        }
+    }
+
+
     @Test
-    public void test(){
-        
+    public void loginSuccess(){
+       app.getHelperUser().openLoginRegistrationForm();
+       app.getHelperUser().fillLoginRegistrationForm("inna_83@gmail.com","Aa13579$");
+       app.getHelperUser().submitLogin();
+       // time ---> signOut
+
+        //Assert is SignOut present
+        //Assert.assertEquals(,);
+        //Assert.assertNotEquals();
+        //Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
+
+
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("inna_83@gmail.com","Aa13579$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
     }
 }
